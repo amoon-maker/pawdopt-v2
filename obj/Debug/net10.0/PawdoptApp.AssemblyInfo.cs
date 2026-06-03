de72ba85440f9fb011fb9d68112b4a1ed9a27aa7
isabela <isasierra225@gmail.com>
2026-06-03 10:27:28 -0400

    .

--- obj/Debug/net10.0/PawdoptApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/PawdoptApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PawdoptApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a041193f5aacec171b4c917b5593fa8fe53f4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PawdoptApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PawdoptApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
