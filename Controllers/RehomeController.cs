7504f9ddb3d199726b95d0eba3ab48814deb2bd6
isabela <isasierra225@gmail.com>
2026-06-04 14:29:39 -0400

    rehome.

--- Controllers/RehomeController.cs
+++ Controllers/RehomeController.cs
@@ -23,6 +23,77 @@ public class RehomeController : Controller
     [HttpPost]
     public IActionResult SaveStep2()
     {
-        return RedirectToAction("Images");
+        return RedirectToAction("Character");
+    }
+
+    public IActionResult Character()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SaveStep3()
+    {
+        return RedirectToAction("KeyFacts");
+    }
+
+    public IActionResult KeyFacts()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SaveStep4()
+    {
+        return RedirectToAction("Location");
+    }
+
+    public IActionResult Location()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SaveStep5()
+    {
+        return RedirectToAction("Story");
+    }
+
+    public IActionResult Story()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SaveStep6()
+    {
+        return RedirectToAction("Documents");
+    }
+
+    public IActionResult Documents()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SaveStep7()
+    {
+        return RedirectToAction("Confirm");
+    }
+
+    public IActionResult Confirm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Publish()
+    {
+        return RedirectToAction("Success");
+    }
+
+    public IActionResult Success()
+    {
+        return View();
     }
 }
