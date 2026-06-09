9e43f98cfe82bf66a0e3900141a893e51919b318
Amna <amnaayad12.19@gmail.com>
2026-06-09 14:08:28 -0400

    care guide added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult CareGuide()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
